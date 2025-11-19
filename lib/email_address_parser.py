ab89c62a6fc8f8c227e8c2c229ef108a72b858c5
Collins Kiaritha <thuocollins020@gmail.com>
2025-11-19 03:23:19 +0300

    Implement EmailAddressParser class

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -5,9 +5,9 @@ class EmailAddressParser:
         self.string = string
 
     def parse(self):
-        # Regex pattern to match email addresses
+        
         email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-        # Find all matches
+     
         emails = re.findall(email_pattern, self.string)
-        # Return unique emails sorted alphabetically
+        
         return sorted(set(emails))
